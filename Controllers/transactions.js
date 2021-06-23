a63e51c35d026337651796d046e324ca1d12a192
Dequisa <Dequisas@gmail.com>
2021-06-23 16:15:47 -0400

    updating code for heroku

--- Controllers/transactions.js
+++ Controllers/transactions.js
@@ -1,25 +1,25 @@
 const transactions = require("express").Router();
 const transactionsArray = require("../Models/transaction")
 
-transactions.get("/",(request, response)=>{
+transactions.get("/", (request, response) => {
     response.json(transactionsArray)
 });
 
-transactions.get("/:index",(request, response)=>{
+transactions.get("/:index", (request, response) => {
     response.json(transactionsArray[request.params.index])
 });
 
-transactions.post("/",(request, response)=>{
+transactions.post("/", (request, response) => {
     transactionsArray.push(request.body);
-    response.json(transactionsArray[transactionsArray.length -1])
+    response.json(transactionsArray)
 });
 
-transactions.delete("/index",(request, response)=>{
+transactions.delete("/:index", (request, response) => {
     const deleteTransaction = transactionsArray.splice(request.params.index, 1)
     response.json(deleteTransaction);
 });
 
-transactions.put("/index",(request, response)=>{
+transactions.put("/:index", (request, response) => {
     transactionsArray[request.params.index] = request.body;
     response.json(transactionsArray[request.params.index])
 });
